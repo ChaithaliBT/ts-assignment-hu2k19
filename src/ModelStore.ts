96a85887b85a9353294cc528a6000664a9b4c7f0
Nikhil Satish <nikhil.satish@hashedin.com>
2019-02-10 18:41:11 +0530

    completed model store implementation

--- src/ModelStore.ts
+++ src/ModelStore.ts
@@ -2,18 +2,23 @@ export class ModelStore<M extends Model> {
     private data: M[];
 
     getById(id: number): M {
+        return this.filter(m => m.id === id)[0];
     }
 
     filter(predicate: (obj: M) => boolean): M[] {
+        return this.data.filter(predicate);
     }
 
     add(obj: M): void {
+        this.data.push(obj);
     }
 
     remove(id: number): void {
+        this.data = this.data.filter(m => m.id !== id);
     }
 
     update(obj: M): void {
+        this.data = this.data.map(m => obj.id === m.id ? obj : m);
     }
 
 }
