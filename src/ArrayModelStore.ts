fb024be770ec790ba33716d0f031da7326a85c79
Nikhil Satish <nikhil.satish@hashedin.com>
2019-02-10 19:00:34 +0530

    made ModelStore interface and ArrayModelStore class

---
+++ src/ArrayModelStore.ts
@@ -0,0 +1,26 @@
+import { ModelStore } from './ModelStore';
+
+export class ArrayModelStore<M extends Model> implements ModelStore<M> {
+    private data: M[];
+
+    getById(id: number): M {
+        return this.filter(m => m.id === id)[0];
+    }
+
+    filter(predicate: (obj: M) => boolean): M[] {
+        return this.data.filter(predicate);
+    }
+
+    add(obj: M): void {
+        this.data.push(obj);
+    }
+
+    remove(id: number): void {
+        this.data = this.data.filter(m => m.id !== id);
+    }
+
+    update(obj: M): void {
+        this.data = this.data.map(m => obj.id === m.id ? obj : m);
+    }
+
+}
--- src/ModelStore.ts
+++ src/ModelStore.ts
@@ -1,24 +1,12 @@
-export class ModelStore<M extends Model> {
-    private data: M[];
+export interface ModelStore<M extends Model> {
+    getById(id: number): M;
 
-    getById(id: number): M {
-        return this.filter(m => m.id === id)[0];
-    }
+    filter(predicate: (obj: M) => boolean): M[];
 
-    filter(predicate: (obj: M) => boolean): M[] {
-        return this.data.filter(predicate);
-    }
+    add(obj: M): void;
 
-    add(obj: M): void {
-        this.data.push(obj);
-    }
+    remove(id: number): void;
 
-    remove(id: number): void {
-        this.data = this.data.filter(m => m.id !== id);
-    }
-
-    update(obj: M): void {
-        this.data = this.data.map(m => obj.id === m.id ? obj : m);
-    }
+    update(obj: M): void;
 
 }
